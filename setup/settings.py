27789b2db2ad11d9c255a038b3191f79202070da
Maria Beatriz Moreira <mariabeatrizmoreira20@gmail.com>
2025-03-18 11:09:57 -0300

    add BD

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'setup/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
